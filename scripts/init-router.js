e9d18c64a2b9d0c0dc6e9f10862448df561d6ee5
oreshetnyk <neo343612@gmail.com>
2023-12-22 18:59:28 +0200

    lab_3

--- scripts/init-router.js
+++ scripts/init-router.js
@@ -1,15 +1,24 @@
-sh.addShard("rs-shard-01/shard01-a:27017")
-sh.addShard("rs-shard-01/shard01-b:27017")
-sh.addShard("rs-shard-01/shard01-c:27017")
+// init-router.js
+sh.addShard("shard01/shard01a:27018")
+sh.addShard("shard01/shard01b:27018")
+sh.addShard("shard01/shard01c:27018")
 
-sh.addShard("rs-shard-02/shard02-a:27017")
-sh.addShard("rs-shard-02/shard02-b:27017")
-sh.addShard("rs-shard-02/shard02-c:27017")
+sh.addShard("shard02/shard02a:27019")
+sh.addShard("shard02/shard02b:27019")
+sh.addShard("shard02/shard02c:27019")
 
-sh.addShard("rs-shard-03/shard03-a:27017")
-sh.addShard("rs-shard-03/shard03-b:27017")
-sh.addShard("rs-shard-03/shard03-c:27017")
+sh.addShard("shard03/shard03a:27020")
+sh.addShard("shard03/shard03b:27020")
+sh.addShard("shard03/shard03c:27020")
 
-sh.addShard("rs-shard-04/shard04-a:27017")
-sh.addShard("rs-shard-04/shard04-b:27017")
-sh.addShard("rs-shard-04/shard04-c:27017")
+sh.addShard("shard04/shard04a:27021")
+sh.addShard("shard04/shard04b:27021")
+sh.addShard("shard04/shard04c:27021")
+
+// Add admin user
+db = db.getSiblingDB('admin');
+db.createUser({
+  user: "admin",
+  pwd: "admin",
+  roles: ["root"]
+});
--- scripts/init-shard02.js
+++ scripts/init-shard02.js
@@ -1,11 +1,13 @@
+//init-shard02.js
+
 rs.initiate(
    {
-      _id: "rs-shard-02",
+      _id: "shard02",
       version: 1,
       members: [
-         { _id: 0, host : "shard02-a:27017" },
-         { _id: 1, host : "shard02-b:27017" },
-		 { _id: 2, host : "shard02-c:27017" },
+         { _id: 0, host : "shard02a:27019" },
+         { _id: 1, host : "shard02b:27019" },
+         { _id: 2, host : "shard02c:27019" },
       ]
    }
 )
